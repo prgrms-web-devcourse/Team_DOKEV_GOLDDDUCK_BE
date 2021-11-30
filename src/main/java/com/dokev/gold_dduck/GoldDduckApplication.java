4d31d18f609ef9c89def7f3e3709ed0f422fd712
choiyoungkwon12 <hpwh4457@naver.com>
2021-11-30 17:24:08 +0900

    project init

---
+++ src/main/java/com/dokev/gold_dduck/GoldDduckApplication.java
@@ -0,0 +1,13 @@
+package com.dokev.gold_dduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldDduckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldDduckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dokev/gold_dduck/GoldDduckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dokev.gold_dduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldDduckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
