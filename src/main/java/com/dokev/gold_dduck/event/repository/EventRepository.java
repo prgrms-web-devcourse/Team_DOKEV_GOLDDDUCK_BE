a62cbea490d63a0fd70f7a840f8f926400d7c104
GetMapping <eonju.dev@gmail.com>
2021-12-02 12:12:17 +0900

    [GD-90] feat : EventService 구현

--- src/main/java/com/dokev/gold_dduck/event/repository/EventRepository.java
+++ src/main/java/com/dokev/gold_dduck/event/repository/EventRepository.java
@@ -1,7 +1,17 @@
 package com.dokev.gold_dduck.event.repository;
 
 import com.dokev.gold_dduck.event.domain.Event;
+import com.dokev.gold_dduck.gift.domain.Gift;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    Event findEventByCode(@Param("code") UUID code);
+
+    @Query(value = "SELECT a FROM Gift AS a WHERE a.event = :eventId")
+    List<Gift> findGiftsByEventId(Long eventId);
 }
