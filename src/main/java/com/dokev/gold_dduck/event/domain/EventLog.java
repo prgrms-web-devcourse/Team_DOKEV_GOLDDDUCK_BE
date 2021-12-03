62763315119a5af9908dd600cb1a8e5fb561c991
maenguin <49011919+maenguin@users.noreply.github.com>
2021-12-03 16:52:05 +0900

    [GD-94] feat : EventLog 엔티티 생성자 추가 (#10)

--- src/main/java/com/dokev/gold_dduck/event/domain/EventLog.java
+++ src/main/java/com/dokev/gold_dduck/event/domain/EventLog.java
@@ -4,10 +4,14 @@ import com.dokev.gold_dduck.common.BaseEntity;
 import com.dokev.gold_dduck.gift.domain.Gift;
 import com.dokev.gold_dduck.gift.domain.GiftItem;
 import com.dokev.gold_dduck.member.domain.Member;
+import java.util.Objects;
+import lombok.AccessLevel;
 import lombok.Getter;
 
 import javax.persistence.*;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Table(name = "event_log")
 @Entity
@@ -33,4 +37,14 @@ public class EventLog extends BaseEntity {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "gift_item_id")
     private GiftItem giftItem;
+
+    public EventLog(Event event, Member member, Gift gift, GiftItem giftItem) {
+        Objects.requireNonNull(event, "event는 notnull이어야 합니다.");
+        Objects.requireNonNull(member, "member는 notnull이어야 합니다.");
+
+        this.event = event;
+        this.member = member;
+        this.gift = gift;
+        this.giftItem = giftItem;
+    }
 }
