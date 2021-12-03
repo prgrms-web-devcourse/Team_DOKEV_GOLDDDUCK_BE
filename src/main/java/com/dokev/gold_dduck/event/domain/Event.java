02c10c594f698660eb315a0a6b5ee8618fe7593e
choiyoungkwon12 <hpwh4457@naver.com>
2021-12-03 16:41:46 +0900

    [GD-93] feat : Event, Gift, GiftItem에 대한 생성자 또는 빌더 작성

--- src/main/java/com/dokev/gold_dduck/event/domain/Event.java
+++ src/main/java/com/dokev/gold_dduck/event/domain/Event.java
@@ -3,17 +3,31 @@ package com.dokev.gold_dduck.event.domain;
 import com.dokev.gold_dduck.common.BaseEntity;
 import com.dokev.gold_dduck.gift.domain.Gift;
 import com.dokev.gold_dduck.member.domain.Member;
-import lombok.Getter;
-
-import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "event")
 public class Event extends BaseEntity {
 
@@ -23,7 +37,7 @@ public class Event extends BaseEntity {
     private Long id;
 
     @Enumerated(value = EnumType.STRING)
-    @Column(name = "gift_choice_type", length = 20)
+    @Column(name = "gift_choice_type", length = 20, nullable = false)
     private GiftChoiceType giftChoiceType;
 
     @Column(name = "start_at", columnDefinition = "TIMESTAMP")
@@ -39,10 +53,10 @@ public class Event extends BaseEntity {
     @Column(name = "progress_status", nullable = false, length = 30)
     private EventProgressStatus eventProgressStatus;
 
-    @Column(name = "main_template", length = 30)
+    @Column(name = "main_template", length = 30, nullable = false)
     private String mainTemplate;
 
-    @Column(name = "max_participant_count")
+    @Column(name = "max_participant_count", nullable = false)
     private Integer maxParticipantCount;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -51,6 +65,32 @@ public class Event extends BaseEntity {
 
     @OneToMany(mappedBy = "event")
     private List<Gift> gifts = new ArrayList<>();
+
+    @Builder(builderMethodName = "eventInternalBuilder")
+    private Event(GiftChoiceType giftChoiceType, LocalDateTime startAt, LocalDateTime endAt, UUID code,
+        EventProgressStatus eventProgressStatus, String mainTemplate, Integer maxParticipantCount,
+        Member member) {
+        this.giftChoiceType = giftChoiceType;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.code = code;
+        this.eventProgressStatus = eventProgressStatus;
+        this.mainTemplate = mainTemplate;
+        this.maxParticipantCount = maxParticipantCount;
+        this.member = member;
+    }
+
+    public static EventBuilder builder(GiftChoiceType giftChoiceType, LocalDateTime startAt, LocalDateTime endAt,
+        EventProgressStatus eventProgressStatus, String mainTemplate, Integer maxParticipantCount, Member member) {
+        return eventInternalBuilder()
+            .giftChoiceType(giftChoiceType)
+            .startAt(startAt)
+            .endAt(endAt)
+            .eventProgressStatus(eventProgressStatus)
+            .mainTemplate(mainTemplate)
+            .maxParticipantCount(maxParticipantCount)
+            .member(member);
+    }
 
     public void changeMember(Member member) {
         if (Objects.nonNull(this.member)) {
--- src/main/java/com/dokev/gold_dduck/gift/domain/GiftItem.java
+++ src/main/java/com/dokev/gold_dduck/gift/domain/GiftItem.java
@@ -1,6 +1,7 @@
 package com.dokev.gold_dduck.gift.domain;
 
 import com.dokev.gold_dduck.common.BaseEntity;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -12,8 +13,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
 @Table(name = "gift_item")
@@ -37,4 +41,18 @@ public class GiftItem extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "gift_id")
     private Gift gift;
+
+    public GiftItem(GiftType giftType, String content, boolean used) {
+        this.giftType = giftType;
+        this.content = content;
+        this.used = used;
+    }
+
+    public void changeGift(Gift gift) {
+        if (Objects.nonNull(this.gift)) {
+            this.gift.getGiftItems().remove(this);
+        }
+        this.gift = gift;
+        gift.getGiftItems().add(this);
+    }
 }
