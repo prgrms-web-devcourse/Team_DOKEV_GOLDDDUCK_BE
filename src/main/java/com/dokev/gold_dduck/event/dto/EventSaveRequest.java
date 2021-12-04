04a2aa9dd7b19f8f004128f663b98d52e5a9ce4d
choiyoungkwon12 <hpwh4457@naver.com>
2021-12-05 03:07:11 +0900

    [GD-82] feat : 이벤트 생성 요청에 필요한 DTO 작성

---
+++ src/main/java/com/dokev/gold_dduck/event/dto/EventSaveRequest.java
@@ -0,0 +1,20 @@
+package com.dokev.gold_dduck.event.dto;
+
+import com.dokev.gold_dduck.event.domain.GiftChoiceType;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class EventSaveRequest {
+
+    private Long memberId;
+    private List<GiftSaveDto> gifts;
+    private GiftChoiceType giftChoiceType;
+    private String eventThumbnailType;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private int maxParticipantCount;
+}
