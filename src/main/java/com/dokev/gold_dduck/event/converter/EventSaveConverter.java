6f2b4de0251f6cb816efaa6457006f31ec1563e5
choiyoungkwon12 <hpwh4457@naver.com>
2021-12-05 03:11:06 +0900

    [GD-82] feat : 이벤트 생성 시 필요한 컨버터 구현

---
+++ src/main/java/com/dokev/gold_dduck/event/converter/EventSaveConverter.java
@@ -0,0 +1,38 @@
+package com.dokev.gold_dduck.event.converter;
+
+import com.dokev.gold_dduck.event.domain.Event;
+import com.dokev.gold_dduck.event.domain.EventProgressStatus;
+import com.dokev.gold_dduck.event.dto.EventSaveRequest;
+import com.dokev.gold_dduck.event.dto.GiftItemSaveDto;
+import com.dokev.gold_dduck.event.dto.GiftSaveDto;
+import com.dokev.gold_dduck.gift.domain.Gift;
+import com.dokev.gold_dduck.gift.domain.GiftItem;
+import com.dokev.gold_dduck.member.domain.Member;
+import java.util.UUID;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventSaveConverter {
+
+    public Event converterToEvent(EventSaveRequest eventSaveRequest, Member member) {
+        return Event.builder(eventSaveRequest.getGiftChoiceType(),
+                eventSaveRequest.getStartAt(),
+                eventSaveRequest.getEndAt(),
+                EventProgressStatus.RUNNING,
+                eventSaveRequest.getEventThumbnailType(),
+                eventSaveRequest.getMaxParticipantCount(),
+                member)
+            .code(UUID.randomUUID())
+            .build();
+    }
+
+    public Gift converterToGift(GiftSaveDto giftSaveDto) {
+        return new Gift(giftSaveDto.getCategory(), giftSaveDto.getGiftItems().size());
+    }
+
+    public GiftItem converterToGiftItem(GiftItemSaveDto giftItemSaveDto) {
+        return new GiftItem(giftItemSaveDto.getGiftType(),
+            giftItemSaveDto.getContent(),
+            false);
+    }
+}
