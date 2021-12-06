f17b8e56649beee00ea258e4024be3e2886ff528
choiyoungkwon12 <hpwh4457@naver.com>
2021-12-06 15:35:26 +0900

    [GD-82] refactor : 이벤트 생성 시 Converter에서 선물, 선물아이템 함께 생성 후 연관관계 매핑하도록 변경

--- src/main/java/com/dokev/gold_dduck/event/converter/EventSaveConverter.java
+++ src/main/java/com/dokev/gold_dduck/event/converter/EventSaveConverter.java
@@ -8,8 +8,10 @@ import com.dokev.gold_dduck.event.dto.GiftSaveDto;
 import com.dokev.gold_dduck.gift.domain.Gift;
 import com.dokev.gold_dduck.gift.domain.GiftItem;
 import com.dokev.gold_dduck.member.domain.Member;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 @Component
 public class EventSaveConverter {
@@ -31,6 +33,48 @@ public class EventSaveConverter {
     }
 
     public GiftItem convertToGiftItem(GiftItemSaveDto giftItemSaveDto) {
+        return new GiftItem(giftItemSaveDto.getGiftType(),
+            giftItemSaveDto.getContent(),
+            false);
+    }
+
+    public Event convertEventSaveDtoToEvent(EventSaveDto eventSaveRequest, Member member)
+        throws IllegalArgumentException {
+        Event newEvent = Event.builder(eventSaveRequest.getGiftChoiceType(),
+                eventSaveRequest.getStartAt(),
+                eventSaveRequest.getEndAt(),
+                EventProgressStatus.RUNNING,
+                eventSaveRequest.getMainTemplate(),
+                eventSaveRequest.getMaxParticipantCount(),
+                member)
+            .code(UUID.randomUUID())
+            .build();
+
+        List<GiftSaveDto> GiftSaveDtos = eventSaveRequest.getGifts();
+        Assert.notEmpty(GiftSaveDtos, "gifts must not be empty");
+
+        GiftSaveDtos.forEach(giftSaveDto -> {
+            Gift gift = convertGiftSaveDtoToGift(giftSaveDto);
+            gift.changeEvent(newEvent);
+        });
+        return newEvent;
+    }
+
+    public Gift convertGiftSaveDtoToGift(GiftSaveDto giftSaveDto) throws IllegalArgumentException {
+        Gift gift = new Gift(giftSaveDto.getCategory(), giftSaveDto.getGiftItems().size());
+        List<GiftItemSaveDto> giftItemSaveDtos = giftSaveDto.getGiftItems();
+
+        Assert.notEmpty(giftItemSaveDtos, "giftItems must not be empty");
+
+        giftItemSaveDtos.forEach(giftItemSaveDto -> {
+            GiftItem giftItem = convertGiftItemSaveDtoToGiftItem(giftItemSaveDto);
+            giftItem.changeGift(gift);
+        });
+
+        return gift;
+    }
+
+    public GiftItem convertGiftItemSaveDtoToGiftItem(GiftItemSaveDto giftItemSaveDto) {
         return new GiftItem(giftItemSaveDto.getGiftType(),
             giftItemSaveDto.getContent(),
             false);
--- src/main/java/com/dokev/gold_dduck/event/service/EventService.java
+++ src/main/java/com/dokev/gold_dduck/event/service/EventService.java
@@ -5,21 +5,15 @@ import com.dokev.gold_dduck.common.exception.MemberNotFoundException;
 import com.dokev.gold_dduck.event.converter.EventSaveConverter;
 import com.dokev.gold_dduck.event.domain.Event;
 import com.dokev.gold_dduck.event.dto.EventSaveDto;
-import com.dokev.gold_dduck.event.dto.GiftSaveDto;
 import com.dokev.gold_dduck.event.repository.EventRepository;
-import com.dokev.gold_dduck.gift.domain.Gift;
-import com.dokev.gold_dduck.gift.domain.GiftItem;
 import com.dokev.gold_dduck.gift.repository.GiftItemRepository;
 import com.dokev.gold_dduck.gift.repository.GiftRepository;
 import com.dokev.gold_dduck.member.domain.Member;
 import com.dokev.gold_dduck.member.repository.MemberRepository;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Assert;
 
 @Slf4j
 @Service
@@ -47,36 +41,12 @@ public class EventService {
         Member member = memberRepository.findById(eventSaveRequest.getMemberId())
             .orElseThrow(() -> new MemberNotFoundException(eventSaveRequest.getMemberId()));
 
-        Event event = eventConverter.convertToEvent(eventSaveRequest, member);
-
-        List<GiftSaveDto> giftSaveDtos = eventSaveRequest.getGifts();
-
-        List<Gift> gifts = new ArrayList<>();
-        List<GiftItem> giftItems = new ArrayList<>();
-
-        giftSaveDtos.forEach(giftSaveDto -> {
-            Gift gift = eventConverter.convertToGift(giftSaveDto);
-            gift.changeEvent(event);
-            gifts.add(gift);
-            giftSaveDto.getGiftItems()
-                .forEach(giftItemSaveDto -> {
-                    GiftItem giftItem = eventConverter.convertToGiftItem(giftItemSaveDto);
-                    giftItem.changeGift(gift);
-                    giftItems.add(giftItem);
-                });
-        });
-
         try {
-            Assert.notEmpty(giftItems, "giftItems must not be empty");
-            Assert.notEmpty(gifts, "gifts must not be empty");
+            Event newEvent = eventConverter.convertEventSaveDtoToEvent(eventSaveRequest, member);
+            Event createdEvent = eventRepository.save(newEvent);
+            return createdEvent.getCode();
         } catch (IllegalArgumentException e) {
             throw new GiftEmptyException(e.getMessage());
         }
-
-        Event createdEvent = eventRepository.save(event);
-        giftRepository.saveAll(gifts);
-        giftItemRepository.saveAll(giftItems);
-
-        return createdEvent.getCode();
     }
 }
