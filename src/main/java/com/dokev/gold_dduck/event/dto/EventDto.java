7dfd6601ce372eaa0e7b41ab7aecdfe84f5abcd0
GetMapping <eonju.dev@gmail.com>
2021-12-09 21:44:38 +0900

    [GD-136] fix : shape object로 변경 및 포맷 삭제

--- src/main/java/com/dokev/gold_dduck/event/dto/EventDto.java
+++ src/main/java/com/dokev/gold_dduck/event/dto/EventDto.java
@@ -4,6 +4,7 @@ import com.dokev.gold_dduck.event.domain.GiftChoiceType;
 import com.dokev.gold_dduck.gift.dto.GiftDto;
 import com.dokev.gold_dduck.member.dto.MemberDto;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -19,10 +20,10 @@ public class EventDto {
 
     private GiftChoiceType giftChoiceType;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    @JsonFormat(shape = Shape.OBJECT, timezone = "Asia/Seoul")
     private LocalDateTime startAt;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    @JsonFormat(shape = Shape.OBJECT, timezone = "Asia/Seoul")
     private LocalDateTime endAt;
 
     private UUID code;
