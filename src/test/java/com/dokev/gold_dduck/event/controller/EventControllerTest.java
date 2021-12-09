1cbc29b57bda8929b571b699bf53e85e1dcda972
Shin Eonju <eonju.dev@gmail.com>
2021-12-09 14:14:23 +0900

    [GD-101] feat : event 단일 조회 컨트롤러와 테스트 작성하기 (#26)
    * [GD-101] feat : 이벤트 단일 조회 controller 구현
    
    * [GD-101] test : 이벤트 단일 조회 controllerTest 작성
    
    * [GD-101] fix : Converter 오류 수정
    
    * [GD-101] fix : 실패 테스트 오류 수정
    
    * [GD-101] fix : 실패 테스트 status 추가
    
    * [GD-101] fix : 피드백 반영

--- src/test/java/com/dokev/gold_dduck/event/controller/EventControllerTest.java
+++ src/test/java/com/dokev/gold_dduck/event/controller/EventControllerTest.java
@@ -5,19 +5,23 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.matchesPattern;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.dokev.gold_dduck.common.error.ErrorCode;
+import com.dokev.gold_dduck.event.converter.EventFindConverter;
 import com.dokev.gold_dduck.event.converter.EventSaveConverter;
 import com.dokev.gold_dduck.event.domain.Event;
+import com.dokev.gold_dduck.event.dto.EventDto;
 import com.dokev.gold_dduck.event.dto.EventSaveDto;
 import com.dokev.gold_dduck.factory.TestEventFactory;
 import com.dokev.gold_dduck.factory.TestMemberFactory;
 import com.dokev.gold_dduck.member.domain.Member;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.UUID;
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -46,6 +50,9 @@ class EventControllerTest {
     @Autowired
     private EventSaveConverter eventSaveConverter;
 
+    @Autowired
+    private EventFindConverter eventFindConverter;
+
     @Test
     @DisplayName("이벤트 생성 테스트 - 성공")
     void saveEventTest() throws Exception {
@@ -72,6 +79,45 @@ class EventControllerTest {
                 jsonPath("$.data", is(notNullValue())),
                 jsonPath("$.data", matchesPattern("([a-f0-9]{8}(-[a-f0-9]{4}){4}[a-f0-9]{8})"))
             );
+    }
+
+    @Test
+    @DisplayName("이벤트 조회 테스트 - 성공")
+    void findEventByCode() throws Exception {
+        //given
+        Member member = TestMemberFactory.createTestMember();
+        entityManager.persist(member);
+
+        Event event = TestEventFactory.createEvent(member);
+        event.changeMember(member);
+        entityManager.persist(event);
+
+        entityManager.clear();
+
+        EventDto eventDto = eventFindConverter.convertToEventDto(event);
+
+        //when, then
+        mockMvc.perform(get("/api/v1/events/{event-code}", event.getCode()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success", is(true)))
+                .andExpect(jsonPath("$.data.eventId", is(eventDto.getEventId().intValue())))
+                .andExpect(jsonPath("$.data.giftChoiceType", is(eventDto.getGiftChoiceType().toString())))
+                .andExpect(jsonPath("$.data.code", is(eventDto.getCode().toString())))
+                .andExpect(jsonPath("$.data.eventProgressStatus", is(eventDto.getEventProgressStatus().toString())))
+                .andExpect(jsonPath("$.data.mainTemplate", is(eventDto.getMainTemplate())))
+                .andExpect(jsonPath("$.data.maxParticipantCount", is(eventDto.getMaxParticipantCount())))
+                .andExpect(jsonPath("$.data.member.id", is(eventDto.getMember().getId().intValue())))
+                .andExpect(jsonPath("$.data.gifts[0].id", is(eventDto.getGifts().get(0).getId().intValue())))
+                .andExpect(jsonPath("$.data.gifts[1].id", is(eventDto.getGifts().get(1).getId().intValue())))
+                .andExpect(jsonPath("$.data.gifts[2].id", is(eventDto.getGifts().get(2).getId().intValue())))
+                .andExpect(jsonPath("$.data.gifts[0].giftItems[0].id",
+                        is(eventDto.getGifts().get(0).getGiftItems().get(0).getId().intValue())))
+                .andExpect(jsonPath("$.data.gifts[0].giftItems[1].id",
+                        is(eventDto.getGifts().get(0).getGiftItems().get(1).getId().intValue())))
+                .andExpect(jsonPath("$.data.gifts[0].giftItems[2].id",
+                        is(eventDto.getGifts().get(0).getGiftItems().get(2).getId().intValue())))
+                .andExpect(jsonPath("$.error", is(nullValue())));
     }
 
     @Test
@@ -132,5 +178,18 @@ class EventControllerTest {
                 jsonPath("$.error.code", is(ErrorCode.INVALID_INPUT_VALUE.getCode())),
                 jsonPath("$.error.message", containsString(ErrorCode.INVALID_INPUT_VALUE.getMessage()))
             );
+    }
+
+    @Test
+    @DisplayName("이벤트 조회 테스트 - 실패")
+    void findEventByCodeFailTest() throws Exception {
+        mockMvc.perform(get("/api/v1/events/{event-code}", UUID.randomUUID()))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.success", is(false)))
+                .andExpect(jsonPath("$.data", is(nullValue())))
+                .andExpect(jsonPath("$.error.code", is(ErrorCode.ENTITY_NOT_FOUND.getCode())))
+                .andExpect(jsonPath("$.error.message",
+                        containsString("해당 엔티티를 찾을 수 없습니다.")));
     }
 }
