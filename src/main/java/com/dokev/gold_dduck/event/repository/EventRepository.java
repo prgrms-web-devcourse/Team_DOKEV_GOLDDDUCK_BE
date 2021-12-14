c5950c3b7dffd0ad69cbe45d231ca273566a0080
GetMapping <eonju.dev@gmail.com>
2021-12-14 22:01:55 +0900

    [GD-166] fix : transactional 추가, member에 대한 fetch join 추가

--- src/main/java/com/dokev/gold_dduck/event/repository/EventRepository.java
+++ src/main/java/com/dokev/gold_dduck/event/repository/EventRepository.java
@@ -3,14 +3,17 @@ package com.dokev.gold_dduck.event.repository;
 import com.dokev.gold_dduck.event.domain.Event;
 import java.util.Optional;
 import java.util.UUID;
+import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface EventRepository extends JpaRepository<Event, Long>, EventRepositoryCustom {
 
+    @Transactional
     @Query("select distinct e from Event e"
         + " join fetch e.gifts"
+        + " join fetch e.member"
         + " where e.code = :eventCode AND e.deletedAt IS null")
     Optional<Event> findEventByCodeWithGift(@Param("eventCode") UUID eventCode);
 
