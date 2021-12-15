7be75fb9a65adb49671bff7308b060bfa8a5b118
maenguin <maenguin924@gmail.com>
2021-12-15 16:55:44 +0900

    chore : cors dev 프로파일 제거

--- src/main/java/com/dokev/gold_dduck/config/WebMvcConfiguration.java
+++ src/main/java/com/dokev/gold_dduck/config/WebMvcConfiguration.java
@@ -1,11 +1,9 @@
 package com.dokev.gold_dduck.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Profile("dev")
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
