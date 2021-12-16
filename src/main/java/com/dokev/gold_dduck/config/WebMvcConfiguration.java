046ea03f2eaa794658f4a3d2cf0d5e40d0378079
maenguin <maenguin924@gmail.com>
2021-12-16 22:41:25 +0900

    chore : cors 설정 변경

--- src/main/java/com/dokev/gold_dduck/config/WebMvcConfiguration.java
+++ src/main/java/com/dokev/gold_dduck/config/WebMvcConfiguration.java
@@ -1,6 +1,7 @@
 package com.dokev.gold_dduck.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +11,12 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("*");
+            .allowedOrigins("*")
+            .allowedMethods(
+                HttpMethod.GET.name(),
+                HttpMethod.HEAD.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name());
     }
 }
